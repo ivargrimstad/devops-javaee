e120ffba33e3a83a98ba82d0693b8ffd1867cbea
Ivar Grimstad <ivar.grimstad@gmail.com>
2015-03-14 13:16:30 +0100

    switched spring boot example to using @EnableEurekaClient rather than just @EnableDiscoveryClient

--- snoop-spring-eureka/snoop-spring-eureka-client/src/main/java/eu/agilejava/eureka/Application.java
+++ snoop-spring-eureka/snoop-spring-eureka-client/src/main/java/eu/agilejava/eureka/Application.java
@@ -25,14 +25,14 @@ package eu.agilejava.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *
  * @author Ivar Grimstad <ivar.grimstad@gmail.com>
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class Application {
 
    public static void main(String[] args) throws Exception {
